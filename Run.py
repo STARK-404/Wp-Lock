4467cfd7984ba7028680d6b1b9df7387e88262b2
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-08-04 20:19:27 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,12 +1,28 @@
+
+# ---------------- Imports
 import os
-import zipfile
-import time
-import rich
+import shutil
+
+try:
+    import pyzipper
+    import colorama
+    from rich import print
+    from rich.panel import Panel
+    import re
+except ImportError:
+    _ = os.system('pip install colorama' if os.name == 'nt' else 'pip3 install colorama')
+    _ = os.system('pip install pyzipper' if os.name == 'nt' else 'pip3 install pyzipper')
+    _ = os.system('pip install rich' if os.name == 'nt' else 'pip3 install rich')
+
+import pyzipper
+import sys
+from time import sleep
+from getpass import getpass
 from colorama import Fore
 from rich.panel import Panel
-from rich import print
-#colors
-os.system('git pull')
+import os
+
+# -------------------Colors
 r = "\033[1;31m"
 g = "\033[1;32m"
 y = "\033[1;33m"
@@ -16,51 +32,73 @@ R = "\033[1;41m"
 Y = "\033[1;43m"
 B = "\033[1;44m"
 w = "\033[1;37m"
-g = "\033[0;90m"
-os.system("npm install")
-def clr_lgtv ():
-	os.system('cls' if os.name=='nt' else 'clear')
-	
-	
+gr = "\033[0;90m"
+
+y = r
+os.system("clear")
+# ------------------banners
 logo = Panel(""" [bold green]
 \t</> Developer : STARK-404 </>
 \t</> Github : STARK-404 </>
 \t</> INSTA : la1uuuuu </>
 """,width=50,style="bold hot_pink2",title='(Wp-venom)')
-if os.path.exists('index.js'):
-	os.system("node index.js")
-else:
-	pass
+
+def banner():
+    print(logo)
+
+def cls_clear_func():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def exixting_directory_file(file):
+    if os.path.exists(file):
+        os.system("node index.js")
+
+def designprint(samaywrite):
+    print(logo)
+    print(R+"└─> "+d+g+samaywrite)
+    print("[bold green]Please Wait Files Are Extracting ...")
+
+def front_design():
+    cls_clear_func()
+    banner()
+
+front_design()
 
 
-def unzip_file(zip_file, password=None):
+class Setup:
+    def __init__(self,user):
+        self.data = user
+    def mainFile(self):
+        self.save = self.data
+        try:
+            with pyzipper.AESZipFile('venom.zip', 'r', compression=pyzipper.ZIP_DEFLATED,
+                                     encryption=pyzipper.WZ_AES) as extracted_zip:
+                extracted_zip.extractall(pwd=str.encode(self.save))
+            designprint(f'{g}Password Correct !')
+            sleep(2.3)
+            front_design()
+            designprint(f'{g}Extraction Sucessfull..✓')
+            sleep(3.0)
+            exixting_directory_file('venom.zip')
+            os.system('node index.js' if os.name=='nt' else 'node index.js')
+        except Exception as samay:
+            designprint(f'{r}Password Incorrect !')
+            print(f"{gr}[{r}!{gr}]{r}Contact Admin For Password!")
+            os.system("xdg-open mailto:unknownshooter509@gmail.com?Subject='Lock!'&Body='I`m%20to%20Buy!")
+            os.system('python Run.py' if os.name=='nt' else 'python3 Run.py')
 
-    zip_file = os.path.abspath(zip_file)
-    dest_dir = os.path.dirname(zip_file)
 
-    try:
-        with zipfile.ZipFile(zip_file, 'r') as zf:
-            if password:
-                zf.setpassword(password.encode())
-            zf.extractall(dest_dir)
-        print("[bold green]Extraction Sucessfull..✓")
-        time.sleep(0.5)
-        os.system('node index.js')
+try:
+    user_ezip_unzipping = getpass(r+"[ "+g+"× "+r+"]" +g+"Enter password For Decrypt the Zip " +r+">"  ).strip()
+except:
+    pass
 
-    except zipfile.BadZipfile:
-        print("Zip File Error")
-    except RuntimeError:
-        print(f"[bold white][[bold red] •[bold white] ][bold red]Incorrect password. Please try again.")
-        
-        time.sleep(7)
-        os.system("xdg-open mailto:unknownshooter509@gmail.com?Subject='Lock!'&Body='I`m%20to%20Buy!'")
-        clr_lgtv()
-        print(logo)
-        password = input(f"{r}[ {g}× {r}] {g}Enter password For Decrypt the Zip {r}>  ")
-        unzip_file(zip_file, password)
+if __name__ == '__main__':
+    exixting_directory_file('node index.js')
+    main_start = Setup(user_ezip_unzipping)
+    main_start.mainFile()
 
-zip_file = "venom.zip"
-clr_lgtv()
-print(logo)
-password = input(f"{r}[ {g}× {r}] {g}Enter password For Decrypt the Zip {r}>  ")
-unzip_file(zip_file, password)
+
+
+
+

cf7b76c1e32b8ae20551a93229da7dc1ba8314f6
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2024-12-22 15:22:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-apt install git python nodejs -y
-git clone https://github.com/STARK-404/Wp-Lock/
-cd Wp-Lock/
-pip install -r requirment.txt
-node index.js
+
+if [[ -n "$TERMUX_VERSION" ]]; then
+        clear
+        pkg update
+        pkg install python3 nodejs npm -y
+        pip install rich 
+        npm install 
+        python3 main.py
+elif [[ -f "/data/data/com.termux/files/usr/bin/bash" ]]; then
+        clear
+        pkg update
+        pkg install python3 nodejs npm -y
+        pip install rich 
+        npm install 
+        python3 main.py
+else
+   
+    if [[ "$(uname -o)" == "GNU/Linux" ]]; then
+        clear
+        sudo apt update
+        sudo apt install python3 nodejs npm -y
+        pip install rich 
+        npm install 
+        python3 main.py
+
+    else
+        echo "Unknown environment."
+    fi
+fi
+
+
+
+
